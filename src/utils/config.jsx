84480428179a51171538b5616545ffc57cd474c7
Nurmuhammad-Ikromov <nurmuhammadikromov58@gmail.com>
2026-04-07 11:49:04 +0500

    change api

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -3,8 +3,9 @@ import axios from "axios";
 const API = axios.create({
   // baseURL: "https://test-app-d1ps.onrender.com"
   // baseURL: "https://test-pdp-backend.onrender.com",
+  baseURL: "https://test-pdp-backend-ao8r.onrender.com",
   // baseURL: "http://localhost:3000",
-  baseURL: "https://api.pdp-edu.uz/",
+  // baseURL: "https://api.pdp-edu.uz/",
 });
 
 API.interceptors.request.use(
